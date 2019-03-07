a09d86c3a9276a2ebcb60f51a709505486b073fa
haripriyas21 <46144345+haripriyas21@users.noreply.github.com>
2019-03-07 13:49:34 +0530

    Update angular.js

--- angular.js
+++ angular.js
@@ -10,3 +10,12 @@ $http.post('databaseFiles/orgDetails.php').success(function(data){
 $scope.details = data;
 });
 }
+$scope.insertInfo = function(info){
+$http.post('databaseFiles/insertDetails.php',{"name":info.name,"email":info.email,"address":info.address,"gender":info.gender}).success(function(data){
+if (data == true) {
+getInfo();
+// Hide details insertion form
+$('#empForm').css('display', 'none');
+}
+});
+}
